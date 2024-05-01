db5fc066161dbe63dc6542481697dbc8fbc462a2
Pietro Rebecchi <pier@itu.dk>
2024-05-01 13:45:57 +0200

    commented code

--- FinalProject/01_process_images.py
+++ FinalProject/01_process_images.py
@@ -2,6 +2,7 @@
 FYP project imaging
 """
 
+# Import packages
 import os
 from os.path import exists
 import pandas as pd
@@ -12,8 +13,6 @@ from PIL import Image
 # Import our own file that has the feature extraction functions
 from extract_features import extract_features
 
-
-
 #-------------------
 # Main script
 #-------------------
@@ -23,49 +22,46 @@ from extract_features import extract_features
 image_folder_path = r"./images"
 mask_folder_path = r"./masks"
 
-#Where is the raw data
-file_data = 'metadata.csv'
-  
-#Where we will store the features
-file_features = 'features/features_automatic.csv'
+# Where is the raw data
+file_data = "metadata.csv"
 
-#Read meta-data into a Pandas dataframe
+# Where we will store the features
+file_features = "features/features_automatic.csv"
+
+# Read meta-data into a Pandas dataframe
 df = pd.read_csv(file_data)
 
-# Extract image IDs and labels from the data. 
-image_id = list(df['img_id'])
-label = np.array(df['diagnostic'])
+# Extract image IDs and labels from the data
+image_id = list(df["img_id"])
+label = np.array(df["diagnostic"])
 
-# Here you could decide to filter the data in some way (see task 0)
-# For example you can have a file selected_images.csv which stores the IDs of the files you need
-is_melanoma = label == 'MEL'
+# Filter for MEL
+is_melanoma = label == "MEL"
 num_images = len(image_id)
 
-#Make array to store features
-feature_names = ["image_id", "color_variation_span", "color_variation_amount", "asymmetry_dicescore",'veil',"color_annotation","assymetry_annotation"]
+# Make array to store features
+feature_names = ["image_id", "color_variation_span", "color_variation_amount", "asymmetry_dicescore", "veil", "color_annotation", "assymetry_annotation"]
 num_features = len(feature_names)
-features = [] 
- 
+features = []
+
 # Get a list of all files in the directory
 file_list = [i for i in os.listdir(image_folder_path) if i.endswith(".png")]
- 
+
 # Iterate through each file in the directory
 for n,filename in enumerate(file_list):
     print(f"Analysing picture {n}")
     # Create the full path to the image file
     image_path = os.path.join(image_folder_path, filename)
-    
-    #Read in mask as ground truth
+
+    # Read in mask as ground truth
     maskname = filename[:-4] + "_mask.png"
     mask_path = os.path.join(mask_folder_path, maskname)
-       
+
     x = extract_features(image_path, mask_path)
 
     # Store in the variable we created before
     features.append([filename,*x])
 
-        
-#Save the image_id used + features to a file   
-df_features = pd.DataFrame(features, columns=feature_names)     
-df_features.to_csv(file_features, index=False)  
-    
+# Save the image_id used + features to a file
+df_features = pd.DataFrame(features, columns = feature_names)
+df_features.to_csv(file_features, index = False)
--- FinalProject/extract_features.py
+++ FinalProject/extract_features.py
@@ -5,6 +5,8 @@ Created on Wed Apr 19 09:53:20 2023
 
 @author: vech
 """
+
+# Import packages
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -18,147 +20,164 @@ from skimage.util import img_as_float
 from skimage import color
 from PIL import Image
 
+### ASYMMETRY
 
-
-#help function for asmmetry, finds indices of major axis. Given variable should be a mask.
+# Help function for asymmetry, finds indices of major axis. Given variable should be a mask
 def major_axes(mask):
-    #setting counting variable
+    # Setting counting variable
     column_max = 0
     indice_column_max = None
     degree_max = None
-    
-    #setting up loop for 180 degrees:
+
+    # Setting up loop for 180 degrees
     for degree in range(0, 180, 10):
-        
-        #rotating the picture
+
+        # Rotating the picture
         mask_rotated = rotate(mask, degree)
-        
-        #summing up all columns
+
+        # Summing up all columns
         columns_sum = np.sum(mask_rotated, axis = 0)
 
-        #finding indice of largest
+        # Finding indice of largest
         largest_indice = np.argmax(columns_sum)
 
-        #checking if larger than current max:
+        # Checking if larger than current max
         if np.sum(mask_rotated[:, largest_indice]) > column_max:
             column_max = np.sum(mask_rotated[:, largest_indice])
             indice_column_max = largest_indice
             degree_max = degree
-    
-    #when done rotating picture we rotate one last time to the rotation of the major axis
+
+    # When done rotating picture we rotate one last time to the rotation of the major axis
     mask_max = rotate(mask, degree_max)
-    
-    #we then save the corresponding orthogonal major axis
+
+    # We then save the corresponding orthogonal major axis
     indice_row_max = np.argmax(np.sum(mask_max, axis = 1))
-    
-    #we return the indices of the max row and max column as well as the max rotated picture
+
+    # We return the indices of the max row and max column as well as the max rotated picture
     return indice_column_max, indice_row_max, mask_max
 
-#calculates the dice_score for an mask by folding it along two major axes. Given variable should be a mask
+# Calculates the dice_score for a mask by folding it along two major axes. Given variable should be a mask
 def asymmetry_score(mask):
-    #first we find the major axes and the orientation of the image using our previous function
+    # First we find the major axes and the orientation of the image using our previous function
     major_column, major_row, mask_max = major_axes(mask)
-    
-    #we split the columns along the major axis and convert to bool, since float sucks
+
+    # We split the columns along the major axis and convert to bool
     column_half1 = mask_max[:, :major_column].astype(bool)
     column_half2 = mask_max[:, major_column:].astype(bool)
-    
-    #we make sure they have the same dimensions
+
+    # We make sure they have the same dimensions
     if column_half2.shape[1] > column_half1.shape[1]:
-        differense = column_half2.shape[1] - column_half1.shape[1]
-        column_add = np.zeros((column_half2.shape[0], differense))
+        difference = column_half2.shape[1] - column_half1.shape[1]
+        column_add = np.zeros((column_half2.shape[0], difference))
         column_half1 = np.hstack((column_add, column_half1)).astype(bool)
-    
+
     elif column_half2.shape[1] < column_half1.shape[1]:
-        differense = column_half1.shape[1] - column_half2.shape[1]
-        column_add = np.zeros((column_half1.shape[0], differense))
+        difference = column_half1.shape[1] - column_half2.shape[1]
+        column_add = np.zeros((column_half1.shape[0], difference))
         column_half2 = np.hstack((column_half2, column_add)).astype(bool)
-    
-    #we "flip" the image around the axis
+
+    # We "flip" the image around the axis
     column_half2_flipped = column_half2[:, ::-1]
-    
-    #we do the same for the rows
+
+    # We do the same for the rows
     row_half1 = mask_max[:major_row, :].astype(bool)
     row_half2 = mask_max[major_row:, :].astype(bool)
-    
-    #make sure they have the same dimensions
+
+    # Make sure they have the same dimensions
     if row_half2.shape[0] > row_half1.shape[0]:
-        differense = row_half2.shape[0] - row_half1.shape[0]
-        row_add = np.zeros((differense, row_half2.shape[1]))
+        difference = row_half2.shape[0] - row_half1.shape[0]
+        row_add = np.zeros((difference, row_half2.shape[1]))
         row_half1 = np.vstack((row_add, row_half1)).astype(bool)
-        
+
     elif row_half1.shape[0] > row_half2.shape[0]:
-        differense = row_half1.shape[0] - row_half2.shape[0]
-        row_add = np.zeros((differense, row_half1.shape[1]))
+        difference = row_half1.shape[0] - row_half2.shape[0]
+        row_add = np.zeros((difference, row_half1.shape[1]))
         row_half2 = np.vstack((row_half2, row_add)).astype(bool)
-    
-    #and flip it
+
+    # Flip it
     row_half2_flipped = row_half2[::-1, :]
-    
-    #we calculate the overlap
+
+    # We calculate the overlap
     axes0_overlap = np.sum(column_half1 & column_half2_flipped)
     axes1_overlap = np.sum(row_half1 & row_half2_flipped)
     
     dice = (axes0_overlap*2 + axes1_overlap*2)/(np.sum(mask_max)*2)
-    
+
+    # Return dice
     if dice > 0.88:
         return 0, dice
     elif 0.7 < dice < 0.88:
         return 1, dice
     else:
         return 2, dice
-    #return dice
-
 
 ### BLUE-WHITE VEIL
 
 def blue_white_veil(image, mask):
 
+    # Mask with area corresponding to the lesion white and everything else black
     new_mask = mask > 0
+    # Apply new mask to image in order to only have the lesion and everything else black
     lesion_masked = image * new_mask[:, :, None]
 
+    # Crop lesion_masked so that the lesion occupies most of the image area
     lesion_coordinates = np.where(mask != 0)
     min_x, max_x = min(lesion_coordinates[0]), max(lesion_coordinates[0])
     min_y, max_y = min(lesion_coordinates[1]), max(lesion_coordinates[1])
     cropped_lesion = lesion_masked[min_x:max_x, min_y:max_y]
    
+    # Segment the cropped_lesion given the parameters
     segments = slic(cropped_lesion, n_segments = 10, compactness = 5, sigma = 5, start_label = 1)
 
+    # Count of segments containing blue-white veil
     tot_count = 0
+    # Area of segments containing blue-white veil
     tot_area_segment = 0
+    # Area of the lesion
     tot_area = np.sum(new_mask)
 
+    # Number of segments genereted by the slic() function on the cropped lesion
     number_segments = np.unique(segments)
 
+    # Iterate through the segments
     for i in number_segments:
         segment = cropped_lesion.copy()
         segment[segments != i] = 0
 
-        non_black_mask = np.any(segment != [0, 0, 0], axis=2)
+        # Create an image with the segment and everything else black
+        non_black_mask = np.any(segment != [0, 0, 0], axis = 2)
 
+        # Compute the area of the segment
         area = np.sum(non_black_mask)
 
+        # Mean rgb values for the segment
         mean_rgb_values = np.mean(segment[non_black_mask], axis = 0)
         mean_rgb_values_ls = [int(val) for val in np.nan_to_num(mean_rgb_values)]
 
+        # Rnage of acceptable blue RGB values for blue-white veil
         blue = {"min": np.array([50, 50, 106]),
                 "max": np.array([128, 200, 200])}
 
         mean_rgb_values_ls_to_check = np.array([mean_rgb_values_ls])
 
+        # Check if the mean of RGB values is inside the range, if so add 1 to the count of segments containing blue-white veil
+        # and add the area of the segment to the area of segments containing blue-white veil
         is_blue = np.all((mean_rgb_values_ls_to_check >= blue["min"]) & (mean_rgb_values_ls_to_check <= blue["max"]))
         if is_blue:
             tot_count += 1
             tot_area_segment += area
 
+    # Compute the proportion of the total area occupied by blue_white veil
     proportion = round((tot_area_segment / tot_area) * 100 if tot_area > 0 else 0, 3)
 
+    # If the proportion is between 20 and 80 return 1 (is blue-white veil)
     if 20 < proportion < 80:
         return 1
     else:
         return 0
     
-#COLOR VARIATION
+### COLOR VARIATION
+
 def grayifier(rgb: np.ndarray) -> np.ndarray:
     '''------HELPER FUNCTION------
     Turns a picture into grayscale, returns the picture, reduced to a one dimensional grayscale image, float values'''
@@ -173,7 +192,7 @@ def crop_n_segment(picture: np.ndarray, mask:np.ndarray) -> np.ndarray:
     min_y: np.int64 = min(lesion_coords[1])
     max_y: np.int64 = max(lesion_coords[1])
     cropped_lesion: np.ndarray = picture[min_x:max_x,min_y:max_y]
-    segments: np.ndarray = slic(cropped_lesion, n_segments=20, compactness=15,sigma=5, enforce_connectivity=True,convert2lab=True)
+    segments: np.ndarray = slic(cropped_lesion, n_segments = 20, compactness = 15,sigma = 5, enforce_connectivity = True, convert2lab = True)
     out: np.ndarray = np.empty_like(cropped_lesion)
     out = color.label2rgb(segments,cropped_lesion, kind = "avg")
     return out
@@ -184,7 +203,7 @@ def histogrammer(image: np.ndarray) -> np.ndarray:
     lesion_feat1:np.ndarray = image[image>0]
     minimum: np.float64 = np.mean(lesion_feat1) - 2*np.std(lesion_feat1)
     maximum: np.float64 = np.mean(lesion_feat1) + 2*np.std(lesion_feat1)
-    colr2:np.ndarray = np.histogram(lesion_feat1[(minimum <= lesion_feat1)&(lesion_feat1<=maximum)],bins = 30,range=(0,255))[0] #a bin is a uniformly increasing value
+    colr2:np.ndarray = np.histogram(lesion_feat1[(minimum <= lesion_feat1)&(lesion_feat1 <= maximum)], bins = 30, range = (0,255))[0] #a bin is a uniformly increasing value
     return colr2
 
 def get_range(histo:np.ndarray) ->list[int]:
@@ -203,36 +222,36 @@ def get_range(histo:np.ndarray) ->list[int]:
 def color_variation(image, mask):
     '''------Main FUNCTION------
     gets a numerical int value for the color variation of the lesion between 0 and 2'''
-    # workaround: list[np.ndarray] = get_img(os.path.splitext(indv)[0])
+    # Workaround: list[np.ndarray] = get_img(os.path.splitext(indv)[0])
     img1: np.ndarray = image
     mask1: np.ndarray = mask
 
     prepped_img:np.ndarray = crop_n_segment(img1, mask1)
 
-    #Getting the pixel intensity
+    # Getting the pixel intensity
     plotted_gray:np.ndarray = grayifier(prepped_img.copy())
     historgram:np.ndarray = histogrammer(plotted_gray)
     gray_list:list[int] = get_range(historgram)
-    gray_val:int = min([0,1,2], key=lambda x:abs(x-((gray_list[0]/gray_list[1]*gray_list[1]/3)-1)))
+    gray_val:int = min([0, 1, 2], key=lambda x:abs(x-((gray_list[0]/gray_list[1]*gray_list[1]/3)-1)))
     return [gray_val, *gray_list]
 
-#Main function to extract features from an image, that calls other functions    
+# Main function to extract features from an image, that calls other functions
 def extract_features(image_path, mask_path):
 
     # Read the image
     image_png = Image.open(image_path)
     image_loaded = np.array(image_png.convert("RGB"))
-    
-    #Read in mask as ground truth
+
+    # Read in mask as ground truth
     mask_loaded = plt.imread(mask_path)
 
-    #Function for calculating the asymmetry index
+    # Function for calculating the asymmetry index
     asymmetry_score1, dice_score = asymmetry_score(mask_loaded)
 
-    #Function for calculating the color variation
+    # Function for calculating the color variation
     color_variation1, gray_span, gray_col = color_variation(image_loaded, mask_loaded)
 
-    #Function for calculating blue_white veil
+    # Function for calculating blue_white veil
     blue_white_veil1 = blue_white_veil(image_loaded, mask_loaded)
-    
-    return np.array([gray_span, gray_col,dice_score, blue_white_veil1,asymmetry_score1,color_variation1], dtype=np.int16)
+
+    return np.array([gray_span, gray_col,dice_score, blue_white_veil1,asymmetry_score1,color_variation1], dtype = np.int16)
