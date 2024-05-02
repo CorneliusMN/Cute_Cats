ab00697122368c3fec558b7a2478ce585ba26ae0
szonjauley <szul@itu.dk>
2024-05-02 21:10:15 +0200

    Upload final classifier evaluator code

--- FinalProject/03_evaluate_classifier.py
+++ FinalProject/03_evaluate_classifier.py
@@ -7,16 +7,16 @@ from extract_features import extract_features #our feature extraction
 # The function that classifies new images
 def classify(img, mask):
     
-     #Extract features (only take in first 4 values, as last two are "dumbed down scores")
+     # Extract features (only takes in first 4 values, as last two are "dumbed down scores")
      x = extract_features(img, mask)[:4]
      x = x.reshape(1, -1)
          
      
-     #Load the trained classifier
+     # Load the trained classifier
      classifier = pickle.load(open('CuteCats_classifier.sav', 'rb'))
     
     
-     #Use it on this example to predict the label AND posterior probability
+     # Use it on this example to predict the label AND posterior probability
      pred_label = classifier.predict(x)
      pred_prob = classifier.predict_proba(x)
      
@@ -28,8 +28,4 @@ def classify(img, mask):
     
 # The TAs will call the function above in a loop, for external test images/masks
 
-#IMAGE AND MASK SHOULD BE STRINGS FOR FOLDER PATH + FILE NAME, THE EXTRACT_FEATURES WILL READ IT IN FROM THERE
-image = 'images/PAT_340_714_68.png'
-mask = 'masks/PAT_340_714_68_mask.png'
-
-classify(image, mask)
+# IMAGE AND MASK SHOULD BE STRINGS FOR FOLDER PATH + FILE NAME, THE EXTRACT_FEATURES WILL READ IT IN FROM THERE
