c6b497be3af77bdbdb290a9dec30f6e098b1f8b8
szonjauley <szul@itu.dk>
2024-05-02 20:00:23 +0200

    uploaded classifier evaluator code for testing

--- FinalProject/03_evaluate_classifier.py
+++ FinalProject/03_evaluate_classifier.py
@@ -1,24 +1,19 @@
-
-
-import pickle #for loading your trained classifier
-
+import pickle #for loading trained classifier
+import numpy as np
 from extract_features import extract_features #our feature extraction
 
 
 
-# The function that should classify new images. 
-# The image and mask are the same size, and are already loaded using plt.imread
+# The function that classifies new images
 def classify(img, mask):
     
-    
-     #Resize the image etc, if you did that during training
-    
-     #Extract features (the same ones that you used for training)
-     x = extract_features(img, mask)
+     #Extract features (only take in first 4 values, as last two are "dumbed down scores")
+     x = extract_features(img, mask)[:4]
+     x = x.reshape(1, -1)
          
      
      #Load the trained classifier
-     classifier = pickle.load(open('groupXY_classifier.sav', 'rb'))
+     classifier = pickle.load(open('CuteCats_classifier.sav', 'rb'))
     
     
      #Use it on this example to predict the label AND posterior probability
@@ -26,9 +21,15 @@ def classify(img, mask):
      pred_prob = classifier.predict_proba(x)
      
      
-     #print('predicted label is ', pred_label)
-     #print('predicted probability is ', pred_prob)
+     print('predicted label is ', pred_label)
+     print('predicted probability is ', pred_prob)
      return pred_label, pred_prob
  
     
 # The TAs will call the function above in a loop, for external test images/masks
+
+#IMAGE AND MASK SHOULD BE STRINGS FOR FOLDER PATH + FILE NAME, THE EXTRACT_FEATURES WILL READ IT IN FROM THERE
+image = 'images/PAT_340_714_68.png'
+mask = 'masks/PAT_340_714_68_mask.png'
+
+classify(image, mask)
